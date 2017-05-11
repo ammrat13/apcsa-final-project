7b5ca501c2c5df2585de8fa0eade01cf7c3bcf81
ammrat13 <ammrat13@users.noreply.github.com>
2017-05-11 17:24:51 -0500

    Created `Main` class.

---
+++ src/cs/ratnani/Main.java
@@ -0,0 +1,11 @@
+package cs.ratnani;
+
+/**
+ * This class provides the resources to run this application. It contains one
+ * method: `main`, which will do just that.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
