857613f952d2f7e9a0cdaba5f2dfbe218b8d7947
ammrat13 <ammrat13@users.noreply.github.com>
2017-05-11 17:33:52 -0500

    Created `Complex` class.

--- src/cs/ratnani/Main.java
+++ src/cs/ratnani/Main.java
@@ -3,6 +3,9 @@ package cs.ratnani;
 /**
  * This class provides the resources to run this application. It contains one
  * method: `main`, which will do just that.
+ *
+ * @author Ammar Ratnani
+ * @version 0.0
  */
 public class Main {
     public static void main(String[] args){
---
+++ src/cs/ratnani/complex/Complex.java
@@ -0,0 +1,14 @@
+package cs.ratnani.complex;
+
+/**
+ * This class represents a complex number. It has a constructor for the re-im
+ * representation and has methods to access this number's real part, imaginary
+ * part, complex modulus (r), and complex argument (theta). It also has a
+ * method to compute the color of this number.
+ *
+ * @author Ammar Ratnani
+ * @version 0.0
+ */
+public class Complex {
+
+}
